9446e3ab16104cab1ac8ff44fe8aae545f157278
Mat-h-owl <52097675+Mat-h-owl@users.noreply.github.com>
2023-05-31 15:52:48 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fastformer',
+    version='0.1',
+    packages=find_packages(),
+    url='https://github.com/Mat-h-owl/fastformer',
+    author='Josemanuel',
+    author_email='343.agent.343@gmail.com',
+    description='FastFormer',
+)
